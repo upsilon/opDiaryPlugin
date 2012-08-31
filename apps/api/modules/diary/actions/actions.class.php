3593772c6809954b449a8cf583e0d021b8670989
Shunsuke Watanabe <watanabe@craftgear.net>
2012-08-31 10:41:58 +0900

    add directories and files for api

---
+++ apps/api/modules/diary/actions/actions.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+/**
+ * diary actions.
+ *
+ * @package    OpenPNE
+ * @subpackage action
+ * @author     Shunsuke Watanabe <watanabe@craftgear.net>
+ */
+class diaryActions extends opJsonApiActions
+{
+  public function executeList(sfWebRequest $request)
+  {
+    $this->data = 'hogehoge';
+
+    $this->setTemplate('array');
+  }
+
+}
