956734a1d4d2e10688594747d8c3e207fae3ac01
Shunsuke Watanabe <watanabe@craftgear.net>
2012-09-11 13:34:21 +0900

    add test cases to check diary public flag

--- test/functional/api/diaryPostTest.php
+++ test/functional/api/diaryPostTest.php
@@ -7,6 +7,8 @@ $t = new opTestFunctional(new sfBrowser());
 include dirname(__FILE__).'/../../bootstrap/database.php';
 
 $mailAddress = 'sns1@example.com';
+$myFriendMailAddress = 'sns2@example.com';
+$notMyFriendMailAddress = 'sns5@example';
 
 $t->login($mailAddress, 'password');
 $t->setCulture('en');
@@ -14,7 +16,7 @@ $t->setCulture('en');
 $t->info('should be able to post a new diary entry');
 $title = 'テストタイトル';
 $body = 'テスト本文';
-$publicFlag = 1;//全員に公開
+$publicFlag = PluginDiaryTable::PUBLIC_FLAG_SNS;//全員に公開
 $json = $t->post('/diary/post.json',
     array(
       'apiKey'      => 'dummyApiKey',
@@ -115,7 +117,7 @@ $t->info('should be able to edit a existing diary entry');
 $id = 1;
 $title = '編集後のテストタイトル'.time();
 $body = '編集後のテスト本文'.time();
-$publicFlag = 3;//全員に公開
+$publicFlag = PluginDiaryTable::PUBLIC_FLAG_PRIVATE;
 $json = $t->post('/diary/post.json',
     array(
       'id'          => $id,
@@ -134,4 +136,77 @@ $t->test()->is($data['data']['title'], $title, 'should have the same title poste
 $t->test()->is($data['data']['body'], $body, 'should have the same body posted');
 $t->test()->is($data['data']['public_flag'], $publicFlag, 'should have the same publid flag posted');
 $t->test()->ok($data['data']['created_at'], 'should have the date posted');
+
+$t->info('only my friends should be able to view my diary entry for friends');
+$title = '友人のみに公開するテストタイトル';
+$body = '友人のみに公開するテスト本文';
+$publicFlag = PluginDiaryTable::PUBLIC_FLAG_FRIEND;//友人のみに公開
+$json = $t->post('/diary/post.json',
+    array(
+      'apiKey'      => 'dummyApiKey',
+      'title'       => $title,
+      'body'        => $body,
+      'public_flag' => $publicFlag,
+    )
+  )->getResponse()->getContent()
+;
+$postedData = json_decode($json, true);
+
+$t->login($myFriendMailAddress, 'password');
+$json = $t->get('/diary/search.json',
+    array(
+      'apiKey' => 'dummyApiKey',
+      'id'     => $postedData['id']
+    )
+  )->getResponse()->getContent()
+;
+$data = json_decode($json, true);
+$t->test()->is($postedData['id'], $data['data'], 'my friends can read my entries limited tothem.');
+
+$t->login($notMyFriendMailAddress, 'password');
+$json = $t->get('/diary/search.json',
+    array(
+      'apiKey' => 'dummyApiKey',
+      'id'     => $postedData['id']
+    )
+  )->with('response')->begin()
+    ->isStatusCode(400)
+  ->end()
+;
+
+$t->info('only myself should be able to view my private diary entry');
+$title = '秘密のテストタイトル';
+$body = '秘密のテスト本文';
+$publicFlag = PluginDiaryTable::PUBLIC_FLAG_PRIVATE;
+$json = $t->post('/diary/post.json',
+    array(
+      'apiKey'      => 'dummyApiKey',
+      'title'       => $title,
+      'body'        => $body,
+      'public_flag' => $publicFlag,
+    )
+  )->getResponse()->getContent()
+;
+
+$t->login($myFriendMailAddress, 'password');
+$json = $t->get('/diary/search.json',
+    array(
+      'apiKey' => 'dummyApiKey',
+      'id'     => $postedData['id']
+    )
+  )->with('response')->begin()
+    ->isStatusCode(400)
+  ->end()
+;
+
+$t->login($notMyFriendMailAddress, 'password');
+$json = $t->get('/diary/search.json',
+    array(
+      'apiKey' => 'dummyApiKey',
+      'id'     => $postedData['id']
+    )
+  )->with('response')->begin()
+    ->isStatusCode(400)
+  ->end()
+;
 
