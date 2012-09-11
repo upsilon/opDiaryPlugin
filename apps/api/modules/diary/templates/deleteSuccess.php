6a69ff791b82cc430864aec70c8af833d888b478
Shunsuke Watanabe <watanabe@craftgear.net>
2012-09-11 13:41:41 +0900

    revert diary/delete.json response to return member information

--- apps/api/modules/diary/templates/deleteSuccess.php
+++ apps/api/modules/diary/templates/deleteSuccess.php
@@ -1,6 +1,11 @@
 <?php
 use_helper('opDiary');
+$data = array();
+if(isset($diary))
+{
+  $data = op_api_diary($diary);
+}
 return array(
   'status' => 'success',
-  'data' => $diary->getId(),
+  'data' => $data,
 );
