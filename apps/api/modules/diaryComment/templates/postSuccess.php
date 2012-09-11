9ddb2462b20fd9f33492b2d874619e2b5b71ac90
Shunsuke Watanabe <watanabe@craftgear.net>
2012-09-11 11:22:52 +0900

    remove an images field from diary_comment/post.json response

--- apps/api/modules/diaryComment/templates/postSuccess.php
+++ apps/api/modules/diaryComment/templates/postSuccess.php
@@ -2,6 +2,7 @@
 use_helper('opDiary');
 $data = op_api_diary_comment($comment);
 $data['deletable'] = $comment->isDeletable($memberId);
+unset($data['images']);
 
 return array(
   'status' => 'success',
