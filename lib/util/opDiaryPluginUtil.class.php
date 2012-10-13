36766b5ccab1db8d9cfd79f12c534580f12e17a1
Shunsuke Watanabe <watanabe@craftgear.net>
2012-10-13 13:59:39 +0900

    remove rootpath from notification url #37

--- lib/util/opDiaryPluginUtil.class.php
+++ lib/util/opDiaryPluginUtil.class.php
@@ -3,8 +3,7 @@
 class opDiaryPluginUtil
 {
   public static function sendNotification($fromMember, $toMember, $diaryId){
-    $rootPath = sfContext::getInstance()->getRequest()->getRelativeUrlRoot();
-    $url = $rootPath.'/diary/'.$diaryId;
+    $url = 'diary/'.$diaryId;
     
     sfApplicationConfiguration::getActive()->loadHelpers(array('I18N'));
     $message = format_number_choice('[1]1 diary has new comments|(1,Inf]%1% diaries have new comments', array('%1%'=>'1'), 1);
